4663e592e9af2e8b3db42d586ea5b602c95c16f4
Flogert <flogert@rocketmail.com>
2026-04-26 19:57:22 -0400

    Updates

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,14 @@
 // @ts-check
 import tailwindcss from '@tailwindcss/vite';
 import sitemap from '@astrojs/sitemap';
+import vercel from '@astrojs/vercel';
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
 	site: 'https://www.aiwisespaces.com',
+	output: 'server',
+	adapter: vercel(),
 	integrations: [sitemap()],
 	vite: {
 		plugins: [tailwindcss()],
